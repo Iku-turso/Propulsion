0d203a2a85c61c50cc8ff100618b7bb342a396f2
miaspial <mikko.aspiala@digia.com>
2015-12-27 22:19:35 +0200

    Ships now spawn next to each other instead of on top of each other.

--- Game/ShipFactory.js
+++ Game/ShipFactory.js
@@ -1,7 +1,10 @@
 ﻿ShipFactory = function(world, physics, locate, gameObjects) {
     var self = this;
+    var x = -1;
     self.create = function() {
         var ship = locate('ship');
+        ship.x = x;
+        x += 2;
         world.add(ship);
         physics.add(ship);
         gameObjects.push(ship);
