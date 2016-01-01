1446e34a9364a17436b8b9628cd1df7d73fd57b2
miaspial <mikko.aspiala@digia.com>
2016-01-01 23:59:13 +0200

    The game can now be hosted with node.js and express in addition to IIS.

---
+++ Index.js
@@ -0,0 +1,11 @@
+﻿var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/Index.html');
+});
+
+app.use('/Game', express.static('Game'));
+app.use('/Scripts', express.static('Scripts'));
+
+var server = app.listen(3000);
